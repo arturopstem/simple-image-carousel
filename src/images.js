2fc40b08ffbf889b89e539c224f9dd5eac77c9cc
arturop <arturopstem@gmail.com>
2023-09-09 16:30:24 -0500

    Functional Simple Image Carousel

---
+++ src/images.js
@@ -0,0 +1,33 @@
+import kogmaw from './assets/arcanist_kogmaw.jpg';
+import teemo from './assets/beemo.jpg';
+import warwick from './assets/big_bad_warwick.jpg';
+import galio from './assets/birdio.jpg';
+import tahmkench from './assets/coin_emperor_tahmkench.jpg';
+import fizz from './assets/fuzz_fizz.jpg';
+import gnar from './assets/gentleman_gnar.jpg';
+import rammus from './assets/king_rammus.jpg';
+import amumu from './assets/little_knight_amumu.jpg';
+import maokai from './assets/meowkai.jpg';
+import alistar from './assets/moo_cow_alistar.jpg';
+import sejuani from './assets/poro_rider_sejuani.jpg';
+import rengar from './assets/pretty_kitty_rengar.jpg';
+import braum from './assets/santa_braum.jpg';
+import twitch from './assets/twitch_shadowfoot.jpg';
+
+export {
+  kogmaw,
+  teemo,
+  warwick,
+  galio,
+  tahmkench,
+  fizz,
+  gnar,
+  rammus,
+  amumu,
+  maokai,
+  alistar,
+  sejuani,
+  rengar,
+  braum,
+  twitch,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,85 @@
 import './main.css';
+import angleLeft from './assets/angle-left.svg';
+import angleRight from './assets/angle-right.svg';
+import * as Skins from './images';
+
+const champions = Object.keys(Skins);
+
+const slidesMarkup = champions.reduce((accumulator, element, index) => {
+  const string = `<div class="slide" data-index=${index}><img src="${Skins[element]}"></div>`;
+  return accumulator + string;
+}, '');
+
+const dotsMarkup = champions.reduce((accumulator, element, index) => {
+  const string = `<li class="dot"><button class="dot-btn ${
+    index === 0 ? 'selected' : ''
+  }" data-index=${index}></button></li>`;
+  return accumulator + string;
+}, '');
+
+const markup = `
+<div class="carousel">
+  <div class="slides" data-index="0">${slidesMarkup}</div>
+  <menu class="arrows-menu">
+    <li class="arrow-left"><button class="left-btn">${angleLeft}</button></li>
+    <li class="arrow-right"><button class="right-btn">${angleRight}</button></li>
+  </menu>
+  <menu class="nav-dots">${dotsMarkup}</menu>
+</div>
+`;
+
+const carousel = new DOMParser()
+  .parseFromString(markup, 'text/html')
+  .querySelector('.carousel');
+
+const slides = carousel.querySelector('.slides');
+
+const findSlide = (index) => slides.querySelector(`[data-index="${index}"]`);
+
+const updateNavDots = (index) => {
+  const dotBtns = carousel.querySelectorAll('.dot-btn');
+  dotBtns.forEach((btn) => {
+    if (btn.dataset.index === index.toString()) {
+      btn.classList.add('selected');
+    } else {
+      btn.classList.remove('selected');
+    }
+  });
+};
+
+carousel.addEventListener('click', (e) => {
+  if (e.target.className === 'left-btn') {
+    const currentIndex = Number(slides.dataset.index);
+    const newIndex = (currentIndex - 1 + champions.length) % champions.length;
+    slides.dataset.index = newIndex;
+    const slide = findSlide(newIndex);
+    updateNavDots(newIndex);
+    slide.scrollIntoView();
+  }
+
+  if (e.target.className === 'right-btn') {
+    const currentIndex = Number(slides.dataset.index);
+    const newIndex = (currentIndex + 1) % champions.length;
+    slides.dataset.index = newIndex;
+    const slide = findSlide(newIndex);
+    updateNavDots(newIndex);
+    slide.scrollIntoView();
+  }
+
+  if (e.target.classList.contains('dot-btn')) {
+    const index = Number(e.target.dataset.index);
+    slides.dataset.index = index;
+    const slide = findSlide(index);
+    updateNavDots(index);
+    slide.scrollIntoView();
+  }
+});
+
+document.body.appendChild(carousel);
+
+const timeout = () => {
+  const nextBtn = document.querySelector('.right-btn');
+  nextBtn.click();
+};
+
+setInterval(timeout, 5000);
